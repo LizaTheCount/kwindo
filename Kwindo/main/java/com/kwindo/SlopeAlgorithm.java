0a4e85b2ac767ddf50f0c32ba64589128a6f4be2
Liza de Graaf <liza.de.graaf@hva.nl>
2017-05-13 22:29:35 +0200

    made SlopeAlgorithm

--- Kwindo/main/java/com/kwindo/SlopeAlgorithm.java
+++ Kwindo/main/java/com/kwindo/SlopeAlgorithm.java
@@ -1,11 +1,40 @@
 package com.kwindo;
 
 /**
- * Created by Sijmen on 13-5-2017.
+ * Created by Vincent on 13-5-2017.
  */
 public class SlopeAlgorithm extends KwindoAlgorithm {
+    float prev[] = new float[15];
+    int count = 0;
     @Override
     int runAlgorithm(float stockLevel) {
+        if (count < prev.length && prev[count] == 0.0f) {
+            prev[count] = stockLevel;
+            count++;
+            return 0;
+        }
+
+        float dir = prev[prev.length - 1] - prev[0];
+
+//        System.out.println(dir);
+        float newPrev[] = new float[15];
+
+        int j = 0;
+        for(int i = 1; i < prev.length; i++) {
+            newPrev[j] = prev[i];
+            j++;
+        }
+
+        newPrev[newPrev.length - 1] = stockLevel;
+
+        prev = newPrev;
+
+        if (dir >= +2.5) {
+            return 1;
+        } else if (dir <= -0.001) {
+            return -1;
+        }
+
         return 0;
     }
 }
