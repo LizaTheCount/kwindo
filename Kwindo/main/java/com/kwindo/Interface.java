993341a5c61f08b0e304b34248001741ce05fbb8
Liza de Graaf <liza.de.graaf@hva.nl>
2017-05-14 07:06:07 +0200

    updated Interface code

--- Kwindo/main/java/com/kwindo/Interface.java
+++ Kwindo/main/java/com/kwindo/Interface.java
@@ -1,11 +1,25 @@
 package com.kwindo;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by liza on 5/14/2017.
  */
 public class Interface extends JFrame {
+
+    private ImageIcon image;
+    private JLabel imgLabel;
+
+    Interface() {
+        setLayout(new FlowLayout());
+
+        image = new ImageIcon(getClass().getResource("resources/Kwindo_Logo.png"));
+
+        imgLabel = new JLabel(image);
+        add(imgLabel);
+    }
+
 
     public static void main(String[] args){
 
